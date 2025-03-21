e5235a50e2c535b5d4cd24e97e76d9cdd4891524
daniel79438 <daniel79438@gmail.com>
2025-03-21 17:24:07 +0000

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Daniel Harris on 21/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
