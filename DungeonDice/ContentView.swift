fba652406e39ebfa591b9030f39ab9c1d72c5195
daniel79438 <daniel79438@gmail.com>
2025-03-22 20:28:43 +0000

    Added protocols & Computed Properties

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    enum Dice: Int, CaseIterable {
+    enum Dice: Int, CaseIterable, Identifiable {
+        
         case four = 4
         case six = 6
         case eight = 8
@@ -16,6 +17,14 @@ struct ContentView: View {
         case twelve = 12
         case twenty = 20
         case hundred = 100
+        
+        var id: Int {
+            rawValue // Each rawValue is unique, so it's a good ID
+        }
+        
+        var description: String {
+            String("\(rawValue)-sided")
+        }
         
         func roll() -> Int {
             return Int.random(in: 1...self.rawValue)
@@ -43,9 +52,9 @@ struct ContentView: View {
             Spacer()
             
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
-                    ForEach(Dice.allCases, id: \.self) { dice in
-                        Button("\(dice.rawValue)-sided") {
-                            resultMessage = "You Rolled a \(dice.roll()) on a \(dice.rawValue)- sided dice"
+                    ForEach(Dice.allCases) { dice in
+                        Button(dice.description) {
+                            resultMessage = "You Rolled a \(dice.roll()) on a \(dice)- sided dice"
                         }
                     }
                     .buttonStyle(.borderedProminent)
